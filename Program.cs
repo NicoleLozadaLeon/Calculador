40b310f650876b1246c55dd19452a0719373b550
NicoleLozadaLeon <lozadaleonn@gmail.com>
2024-03-26 21:38:40 -0400

    Proyecto para calcular el precio de un lote.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+            Console.WriteLine("Ingrese el ancho del lote en metros:");
+            double ancho = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el largo del lote en metros:");
+            double largo = Convert.ToDouble(Console.ReadLine());
+
+            double precioPorMetroCuadrado = 10;     
+
+            double area = ancho * largo;
+
+            double precioTotal = area * precioPorMetroCuadrado;
+
+            Console.WriteLine($"El área del lote es: {area} metros cuadrados.");
+            Console.WriteLine($"El precio total del lote es: ${precioTotal}");
+
+
